3f84de5d5aa5004737aa7d880e335e9e0110c353
dimasnugrahs <79310230+dimasnugrahs@users.noreply.github.com>
2026-06-06 16:49:24 +0800

    feat : update profile photo

--- src/components/ProfileComponent.jsx
+++ src/components/ProfileComponent.jsx
@@ -132,7 +132,7 @@ const Profile = () => {
               className="w-full h-full object-cover"
               initial={{ scale: 1.15, opacity: 0 }}
               whileInView={{ scale: 1, opacity: 1 }}
-              viewport={viewportConfig}
+              viewport={viewportConfigImg}
               transition={{ duration: 1.5, ease: [0.25, 1, 0.5, 1] }}
             />
             <div className="absolute inset-0 bg-linear-to-b from-black/70 via-black/40 to-black/80"></div>
