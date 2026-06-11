cad68e472fa16daabe0b0e73284d893df49f31a2
dimasnugrahs <79310230+dimasnugrahs@users.noreply.github.com>
2026-06-11 22:45:31 +0800

    feat : update structure account number

--- src/components/AccountNumberComponent.jsx
+++ src/components/AccountNumberComponent.jsx
@@ -22,12 +22,7 @@ const DigitalEnvelope = () => {
         {/* HEADER & SUBTITLE */}
         <div className="space-y-4">
           <div className="relative inline-block">
-            <h2 className="font-serif text-4xl text-stone-200/90 font-light tracking-wide">
-              Amplop
-            </h2>
-            <h3 className="font-serif text-5xl text-stone-400 italic font-normal -mt-3 ml-12 tracking-wider">
-              Digital
-            </h3>
+            <div>DIGITAL GIFT</div>
           </div>
 
           <p className="text-xs font-light text-zinc-400 leading-relaxed px-4 pt-4 uppercase tracking-[0.18em]">
@@ -50,7 +45,7 @@ const DigitalEnvelope = () => {
                 <span className="w-3 h-1 bg-amber-400 rounded-full"></span>
               </div>
               <span className="text-md font-bold tracking-wider text-blue-400/90 lowercase font-sans">
-                mandırı
+                rekening mandiri
               </span>
             </div>
             <div className="w-8 h-6 bg-zinc-800/80 border border-zinc-700/50 rounded-md opacity-40"></div>
@@ -70,21 +65,7 @@ const DigitalEnvelope = () => {
               onClick={handleCopy}
               className="inline-flex items-center gap-2 bg-stone-800/80 hover:bg-stone-700 text-stone-200 text-xs tracking-wider uppercase px-4 py-2.5 rounded-xl border border-stone-700/50 transition-all shadow-md group-hover:border-zinc-600 cursor-pointer active:scale-95"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-3.5 h-3.5 text-stone-400"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125v-9.75A1.125 1.125 0 0 1 4.875 11.25H8.25m2.25 2.25h9.75c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125v-9.75c0-.621.504-1.125 1.125-1.125"
-                />
-              </svg>
-              Copy No. Rekening
+              Salin Nomor Rekening
             </button>
 
             <AnimatePresence>
